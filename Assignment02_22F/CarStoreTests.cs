d816b2fc203c7befea52da5bd857888d6cd989d5
Hunter-Oswald <hunter-o@outlook.com>
2022-10-16 15:05:56 -0400

    Tests 3, 4, and 5 complete

--- Assignment02_22F/CarStoreTests.cs
+++ Assignment02_22F/CarStoreTests.cs
@@ -43,9 +43,12 @@ namespace Assignment02_22F
             // start at the home page
             driver.Navigate().GoToUrl(Path.Combine(rootPath, "index.html"));
 
+            // press button
             IWebElement buttonHomeToAdmin = driver.FindElement(By.Id("goAdmin"));
             buttonHomeToAdmin.Click();
 
+            // Assert
+            // Make sure on the right page
             Assert.IsTrue(driver.Url.Contains("admin.html"));
             
         }
@@ -57,12 +60,76 @@ namespace Assignment02_22F
             // start at admin page
             driver.Navigate().GoToUrl(Path.Combine(rootPath, "admin.html"));
 
+            // press button
             IWebElement buttonAdminToNewpost = driver.FindElement(By.Id("goNew"));
             buttonAdminToNewpost.Click();
 
+            // Assert
+            // Make sure on the right page
             Assert.IsTrue(driver.Url.Contains("new.html"));
 
         }
 
+        // Test 3 - navigation from cars for sale to home page
+        [TestMethod]
+        public void navigateFromSale_ToHome()
+        {
+            // start at cars for sale
+            driver.Navigate().GoToUrl(Path.Combine(rootPath, "sale.html"));
+
+            // press button
+            IWebElement buttonSaleToHome = driver.FindElement(By.Id("goHome"));
+            buttonSaleToHome.Click();
+
+            // Assert
+            // make sure on the right page
+            Assert.IsTrue(driver.Url.Contains("index.html"));
+        }
+
+        // Test 4 - logging in
+        [TestMethod]
+        public void enterLogin_andSubmit()
+        {
+            // start at the login page
+            driver.Navigate().GoToUrl(Path.Combine(rootPath, "login.html"));
+
+            // enter all the input
+            IWebElement usernameInput = driver.FindElement(By.Id("username"));
+            IWebElement passwordInput = driver.FindElement(By.Id("password"));
+            IWebElement submitButton = driver.FindElement(By.Id("formSubmit"));
+
+            usernameInput.SendKeys("user");
+            passwordInput.SendKeys("pass");
+
+            // press login
+            submitButton.Click();
+
+            // Assert
+            // make sure on the right page; should take you to home page
+            Assert.IsTrue(driver.Url.Contains("index.html"));
+        }
+
+        // Test 5 - loggin in with incorrect information
+        [TestMethod]
+        public void enterLogin_WrongInfo()
+        {
+            // start at the login page
+            driver.Navigate().GoToUrl(Path.Combine(rootPath, "login.html"));
+
+            // enter all the input (incorrect login information)
+            IWebElement usernameInput = driver.FindElement(By.Id("username"));
+            IWebElement passwordInput = driver.FindElement(By.Id("password"));
+            IWebElement submitButton = driver.FindElement(By.Id("formSubmit"));
+
+            usernameInput.SendKeys("false");
+            passwordInput.SendKeys("info");
+
+            // press login
+            submitButton.Click();
+
+            // Assert
+            // make sure on the right page; should leave you at the login page
+            Assert.IsTrue(driver.Url.Contains("login.html"));
+        }
     }
 }
