d65c4cebde0627514df039678f4dcc3421c2e8ad
Hunter-Oswald <hunter-o@outlook.com>
2022-10-16 14:52:12 -0400

    First two tests complete

--- Assignment02_22F/CarStoreTests.cs
+++ Assignment02_22F/CarStoreTests.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
 namespace Assignment02_22F
 {
     [TestClass]
@@ -28,10 +30,39 @@ namespace Assignment02_22F
         }
 
         //Example showing you how to use the rootPath variable to get to any page of the VehicleStore.
-        [TestMethod]
+        /*[TestMethod]
         public void TestExample()
         {
             driver.Navigate().GoToUrl(Path.Combine(rootPath, "login.html"));
+        }*/
+
+        // Test 1 - navigation from the homepage to admin page
+        [TestMethod]
+        public void navigateFromHome_ToAdminPage()
+        {
+            // start at the home page
+            driver.Navigate().GoToUrl(Path.Combine(rootPath, "index.html"));
+
+            IWebElement buttonHomeToAdmin = driver.FindElement(By.Id("goAdmin"));
+            buttonHomeToAdmin.Click();
+
+            Assert.IsTrue(driver.Url.Contains("admin.html"));
+            
         }
+
+        // Test 2 - navigation from admin to new postings
+        [TestMethod]
+        public void navigateFromAdmin_ToPostNewListings()
+        {
+            // start at admin page
+            driver.Navigate().GoToUrl(Path.Combine(rootPath, "admin.html"));
+
+            IWebElement buttonAdminToNewpost = driver.FindElement(By.Id("goNew"));
+            buttonAdminToNewpost.Click();
+
+            Assert.IsTrue(driver.Url.Contains("new.html"));
+
+        }
+
     }
 }
